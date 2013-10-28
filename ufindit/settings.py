74fc11d5f4339f5c6445ce1ba0bc06a1a5b04c87
DenisSavenkov <denis.savenkov@emory.edu>
2013-10-27 23:28:50 -0400

    Adding index page text and game rules

--- ufindit/settings.py
+++ ufindit/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
     if DEBUG else {
         'ENGINE':   'django.db.backends.postgresql_psycopg2',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME':     'ufindit',                                  # Or path to database file if using sqlite3.
-        'USER':     'uFindIt',
+        'USER':     'ufindit',
         'PASSWORD': 'IrLabUFindItPasswd',
         'HOST':     '127.0.0.1',                                # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT':     '5432',                                     # Set to empty string for default.
