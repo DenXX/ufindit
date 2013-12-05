3892afc8d78777f64f9a896ee7a6346eb70a3d65
Denis Savenkov <dsavenk@tritanium.mathcs.emory.edu>
2013-12-05 13:04:53 -0500

    Adding filters to admin view

--- ufindit/admin.py
+++ ufindit/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from ufindit.models import Game, Task, Player, PlayerGame, PlayerTask, Event, Serp, UserSerpResultsOrder
 
+class PlayerTaskAdmin(admin.ModelAdmin):
+    list_filter = ('finish', )
+
+class PlayerGameAdmin(admin.ModelAdmin):
+    readonly_fields = ('start',)
+    list_filter = ('finish',)
+
 admin.site.register(Game)
 admin.site.register(Task)
-
 admin.site.register(Player)
-admin.site.register(PlayerGame)
-admin.site.register(PlayerTask)
+admin.site.register(PlayerGame, PlayerGameAdmin)
+admin.site.register(PlayerTask, PlayerTaskAdmin)
 admin.site.register(Event)
 admin.site.register(Serp)
 admin.site.register(UserSerpResultsOrder)
