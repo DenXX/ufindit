16ee82db5e7334ec17a0e43ed23a6e2f999c8dc2
DenisSavenkov <denis.savenkov@emory.edu>
2013-12-05 13:00:36 -0500

    Adding Yandex.Metrika counter

--- ufindit/admin.py
+++ ufindit/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from ufindit.models import Game, Task, Player, PlayerGame, PlayerTask, Event, Serp, UserSerpResultsOrder
 
+class PlayerTaskAdmin(admin.ModelAdmin):
+    list_filter = ('player', )
+
 admin.site.register(Game)
 admin.site.register(Task)
-
 admin.site.register(Player)
 admin.site.register(PlayerGame)
-admin.site.register(PlayerTask)
+admin.site.register(PlayerTask, PlayerTaskAdmin)
 admin.site.register(Event)
 admin.site.register(Serp)
 admin.site.register(UserSerpResultsOrder)
